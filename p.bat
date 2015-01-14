d1011c420f1270c388461f4a71483ce9aa8838af
Doug Ilijev <doug.ilijev@gmail.com>
2015-01-13 20:50:11 -0600

    refactored AboutUs.html

--- p.bat
+++ p.bat
@@ -1,1 +1,8 @@
-haml index.haml index.html
+@echo off
+call haml index.haml index.html
+call haml AboutUs.haml AboutUs.html
+REM call haml Members.haml Members.html
+REM call haml Announcements.haml Announcements.html
+REM call haml Gallery.haml Gallery.html
+REM call haml Calendar.haml Calendar.html
+REM call haml Corporate.haml Corporate.html
