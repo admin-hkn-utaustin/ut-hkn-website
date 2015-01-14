8f2e1b9bed8e7a289d4bebe0804c29aa7deb46e8
Doug Ilijev <doug.ilijev@gmail.com>
2015-01-14 00:02:09 -0600

    Announcements.haml

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 call haml index.haml index.html
 call haml AboutUs.haml AboutUs.html
 call haml Members.haml Members.html
-REM call haml Announcements.haml Announcements.html
+call haml Announcements.haml Announcements.html
 REM call haml Gallery.haml Gallery.html
 REM call haml Calendar.haml Calendar.html
 REM call haml Corporate.haml Corporate.html
