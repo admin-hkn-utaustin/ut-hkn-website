87b770acbfe32e4120dd2faa8ccfd4a323fe0c33
Doug Ilijev <doug.ilijev@gmail.com>
2015-01-14 00:27:44 -0600

    {Calendar,Corporate,Gallery}.haml

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ call haml index.haml index.html
 call haml AboutUs.haml AboutUs.html
 call haml Members.haml Members.html
 call haml Announcements.haml Announcements.html
-REM call haml Gallery.haml Gallery.html
-REM call haml Calendar.haml Calendar.html
-REM call haml Corporate.haml Corporate.html
+call haml Gallery.haml Gallery.html
+call haml Calendar.haml Calendar.html
+call haml Corporate.haml Corporate.html
