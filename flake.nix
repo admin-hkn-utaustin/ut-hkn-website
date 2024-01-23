ff2546445e8b166629d52a41e72b6128ab471d43
Uneeb Sajjad <uneebsajjad5@gmail.com>
2024-01-22 18:20:27 -0600

    Updated website for SP24

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in rec {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+        ];
+      };
+    }
+  );
+}
